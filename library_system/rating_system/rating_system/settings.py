94e73575d7ebaca73f28d76728c8d011623f0ca4
VillainNumberOne <66220826+VillainNumberOne@users.noreply.github.com>
2023-01-10 12:41:15 +0300

    test db connect

--- library_system/rating_system/rating_system/settings.py
+++ library_system/rating_system/rating_system/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "ratings",
         "USER": "postgres",
-        "HOST": "postgres",
+        "HOST": "svc-postgres",
         "PORT": 5432,
         "PASSWORD": "postgres"
     }
